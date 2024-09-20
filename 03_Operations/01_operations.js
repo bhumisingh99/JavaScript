3e96936926c415a403d95fabc8f1b476f8114304
Bhumi Singh <123504695+bhumisingh99@users.noreply.github.com>
2024-09-20 18:16:00 +0000

    operation config done

--- 03_Operations/01_operations.js
+++ 03_Operations/01_operations.js
@@ -31,3 +31,15 @@ let n9 = "9"
 let n10 = 3
 console.log(n7+n8+n9+n10)
 
+//not preferred, consider as a messy codes, reduces readability
+console.log(3+4*5%2)
+console.log(+true)     //o/p : 1
+// console.log(true+)  //o/p : error
+console.log(+"")       //o/p : 0
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+//refer mdn for prefix and postfix & ecma for conversion
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
