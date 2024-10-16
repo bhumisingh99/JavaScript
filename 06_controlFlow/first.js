f5cd7cb8ebdb75977a76b77bd583ad77a1c78f9e
Bhumi Singh <123504695+bhumisingh99@users.noreply.github.com>
2024-10-16 18:09:20 +0000

    logic flow config done

---
+++ 06_controlFlow/first.js
@@ -0,0 +1,32 @@
+// control flow or logic flow
+// <, >, <=, >=, ==, !=, ===, !== : Operators
+
+// if-else
+const isUserLoggedIn = true
+if (isUserLoggedIn) {  // if condition id true, then only the code will execute given inside
+    console.log( "executed" );
+}
+
+const temp = 51
+if( temp<=50 ) {
+    console.log("Less than 50");
+}
+else {
+    console.log("More than 50");
+}
+
+const score = 200
+if( score>100 ) {
+    const power = "fly"
+    console.log(`user power: ${power}`); 
+}
+
+
+
+
+ 
+
+
+
+
+
